bc7402f93839fec31d2c2c45879d30c97bf36d1d
carlcuevas <carl.cuevasn@duocuc.cl>
2025-11-26 20:38:39 -0300

    Add AWS provider and S3 backend configuration

---
+++ infra-chesse-factory/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "cheese-tfstate-global-villalobos"
+    key            = "global/cheese/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-lock-cheese"
+    encrypt        = true
+  }
+}
